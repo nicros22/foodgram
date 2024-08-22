01c3e3f657136cefb813eb5e6f2771ffabccb64c
Никита Пономаренко <nik24012002@gmail.com>
2024-08-22 10:25:10 +0700

    fixed nginx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -130,9 +130,8 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ((BASE_DIR / "static/"),)
-STATIC_ROOT = BASE_DIR / "collected_static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
